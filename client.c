888e948e533629128787d1a9a4575f14de34ddc2
baiza-christian <baiza.christian@gmail.com>
2019-02-20 11:11:34 -0800

    Real update client.c

--- client.c
+++ client.c
@@ -1,82 +1,66 @@
 //Andrew Delgado
 //Christian Baiza
-// Client side C/C++ program to demonstrate Socket programming 
-// Server side C/C++ program to demonstrate Socket programming
-#include <unistd.h>
+// Client side C/C++ program to demonstrate Socket programming
 #include <stdio.h>
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
 #define PORT 8080
+
 int main(int argc, char const *argv[])
 {
-	int server_fd, new_socket, valread;
 	struct sockaddr_in address;
-	int opt = 1;
-	int addrlen = sizeof(address);
+	int sock = 0, valread;
+	struct sockaddr_in serv_addr;
+	char clientMessage[200] = {0};
 	char buffer[1024] = {0};
-	char serverMessage[200] = {0};
-
-	// Creating socket file descriptor
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
-		perror("socket failed");
-		exit(EXIT_FAILURE);
+		printf("\n Socket creation error \n");
+		return -1;
 	}
 
-	// // Forcefully attaching socket to the port 8080
-	// if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-	// 											&opt, sizeof(opt)))
-	// {
-	// 	perror("setsockopt");
-	// 	exit(EXIT_FAILURE);
-	// }
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons( PORT );
+	memset(&serv_addr, '0', sizeof(serv_addr));
 
-	// Forcefully attaching socket to the port 8080
-	if (bind(server_fd, (struct sockaddr *)&address,
-								sizeof(address))<0)
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+
+	// Convert IPv4 and IPv6 addresses from text to binary form
+	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
 	{
-		perror("bind failed");
-		exit(EXIT_FAILURE);
+		printf("\nInvalid address/ Address not supported \n");
+		return -1;
 	}
-	if (listen(server_fd, 3) < 0)
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
 	{
-		perror("listen");
-		exit(EXIT_FAILURE);
-	}
-	if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-					(socklen_t*)&addrlen))<0)
-	{
-		perror("accept");
-		exit(EXIT_FAILURE);
+		printf("\nConnection Failed \n");
+		return -1;
 	}
 
 	while(1)
+{
+	//printf("\nclientMessage length - %ld\n", strlen(clientMessage));
+	if(strlen(clientMessage)> 0)
 	{
-	//printf("\nserverMessage length - %ld\n", strlen(serverMessage));
-	if(strlen(serverMessage)> 0)
-	{
-		memset(serverMessage, 0, sizeof(serverMessage));
+		memset(clientMessage, 0, sizeof(clientMessage));
 		memset(buffer, 0, sizeof(buffer));
 	}
-	//printf("serverMessage length - %ld\n", strlen(serverMessage));
-
-	valread = read( new_socket , buffer, 1024);
-	printf("client: %s\n",buffer );
-
-	// printf("serverMessage length - %ld\n", strlen(serverMessage));
-	// printf("buffer length - %ld\n", strlen(buffer));
+	//printf("clientMessage length - %ld\n", strlen(clientMessage));
+	//printf("clientMessage length - %ld\n", strlen(clientMessage));
+	//printf("buffer length - %ld\n", strlen(buffer));
   printf("Enter Message: ");
-	fgets(serverMessage, 200,stdin);
-	send(new_socket , serverMessage , strlen(serverMessage) , 0 );
+	fgets(clientMessage, 200,stdin);
+	send(sock , clientMessage , strlen(clientMessage) , 0 );
 
-	//memset(serverMessage, 0, sizeof(serverMessage));
+	//memset(clientMessage, 0, sizeof(clientMessage));
 
 
-	}
+	valread = read( sock , buffer, 1024);
+	printf("server: %s\n",buffer );
+
+}
 	return 0;
 }
+
